0a97acdfc448f3814d1d0a191a71c76930d2714b
Seth M Cohen <smcohen82@gmail.com>
2021-06-27 13:02:49 -0400

    implements semantic ui dropdown in reviews filter

--- src/components/ReviewsFilter.js
+++ src/components/ReviewsFilter.js
@@ -1,17 +1,34 @@
-import { Input } from 'semantic-ui-react'
+import { Input, Dropdown } from 'semantic-ui-react'
  
 function ReviewsFilter({ sortTerm, setSortTerm, filterTerm, setFilterTerm }) {
+
+  const sortOptions = [
+    { key: 'default', value: 'default', text: 'Recently Reviewed' },
+    { key: 'oldest', value: 'oldest', text: 'Least Recently Reviewed' },
+    { key: 'year asc', value: 'year asc', text: 'Release Year(Asc)' },
+    { key: 'year desc', value: 'year desc', text: 'Release Year(Desc)' },
+    { key: 'rating asc', value: 'rating asc', text: 'Rating(Asc)' },
+    { key: 'rating desc', value: 'rating desc', text: 'Rating(Desc)' }
+  ]
+
+  const value = sortTerm
+
+  function handleChange(e, { value }){ 
+    setSortTerm(value)
+  }
+
   return (
     <div className="filter-controls">
-      <Input onChange={(e) => setFilterTerm(e.target.value)} value={filterTerm} placeholder="filter by title"/>
-      <select value={sortTerm} onChange={(e) => setSortTerm(e.target.value)}>
-        <option value="default">Most Recent</option>
-        <option value="oldest">Least Recent</option>
-        <option value="year asc">Year Released(Asc)</option>
-        <option value="year desc">Year Released(Desc)</option>
-        <option value="rating asc">Rating(Asc)</option>
-        <option value="rating desc">Rating(Desc)</option>
-      </select>
+      <Input 
+        onChange={(e) => setFilterTerm(e.target.value)} value={filterTerm} 
+        placeholder="filter by title"
+      />
+      <Dropdown 
+        onChange={handleChange}
+        selection
+        options={sortOptions}
+        value={value}
+      />
     </div>
   )
 }
