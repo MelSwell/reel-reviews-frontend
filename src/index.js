c5e40d60dd24f312df42db6143926b68a06c3024
Seth M Cohen <smcohen82@gmail.com>
2021-08-16 23:32:14 -0400

    Changes browser router to hash router to fix 404s in production

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'
 import './assets/css/index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 
 ReactDOM.render(
